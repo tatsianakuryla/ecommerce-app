ae7e5c6ed237d8162b556b90b6e2aa3459fc4c2c
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2025-05-01 18:00:42 +0300

    feat: configure eslint, ts and vite (RSSECOMM-20, RSSECOMM-21, RSSECOMM-22, RSSECOMM-26)

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(({ mode }) => {
+  const isProd = mode === "production";
+
+  return {
+    plugins: [react()],
+    define: {
+      __DEV__: !isProd,
+    },
+    build: {
+      minify: isProd,
+      sourcemap: !isProd,
+    },
+  };
+});
