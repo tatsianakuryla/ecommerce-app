aa2153b707ed4da24e8dceea51dd8b53c0666bc9
Nastia <the_sun.89@mail.ru>
2025-05-04 09:55:35 +0300

    fix:change settings prettierrc (RSSECOMM-23, RSSECOMM-27)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  const isProd = mode === "production";
+  const isProd = mode === 'production';
 
   return {
     plugins: [react()],
