7c5931b13f761c58a323c01576d92bd9f5e66066
Tatsiana Kurilo <tanyaraenok@mail.ru>
2025-05-07 17:30:07 +0200

    config: add chukra ui configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { Provider } from '@/components/ui/provider'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import React from 'react'
@@ -8,7 +9,9 @@ if (rootElement instanceof HTMLElement) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <React.StrictMode>
-      <App />
+      <Provider>
+        <App />
+      </Provider>
     </React.StrictMode>,
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(({ mode }) => {
   const isProd = mode === 'production'
 
   return {
-    plugins: [react()],
+    plugins: [react(), tsconfigPaths()],
     define: {
       __DEV__: !isProd,
     },
